2eba504958ff24ca362734a85e25423b7dfa6da3
herjflorez <hernandojflorez2013@gmail.com>
2024-05-21 18:25:04 +0200

    Initial commit

---
+++ src/main/java/models/tokenRequest.java
@@ -0,0 +1,15 @@
+package models;
+
+public class tokenRequest {
+	private String user;
+	private String token;
+	
+	public String getToken() {
+		return token;
+	}
+	
+	public String getUser() {
+		return user;
+	}
+
+}
